0bedc499b36a5567af1cd25508acf4befb45afbb
Mahbub049 <mahbubsarwar49@gmail.com>
2026-04-29 02:11:24 +0600

    vercel deploy

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,87 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
 import routes from "./routes";
 import { env } from "./config/env";
+import { connectDB } from "./config/db";
+import { seedAdmin } from "./utils/seedAdmin";
+import { seedPages } from "./utils/seedPages";
+import { seedHomepage } from "./utils/seedHomepage";
 
 const app = express();
 
+let isDatabaseReady = false;
+
+const allowedOrigins = [
+  env.clientUrl,
+  "http://localhost:3000",
+  "http://localhost:3001",
+].filter(Boolean);
+
 app.use(
   cors({
-    origin: env.clientUrl,
+    origin: allowedOrigins,
     credentials: true,
   })
 );
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
+app.use(async (_req: Request, _res: Response, next: NextFunction) => {
+  try {
+    if (!isDatabaseReady) {
+      await connectDB();
+
+      await seedAdmin();
+      await seedPages();
+      await seedHomepage();
+
+      isDatabaseReady = true;
+    }
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
 
 app.use("/api", routes);
 
-app.get("/", (_req, res) => {
-  res.send("Server is running");
+app.get("/", (_req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "BUP FST Journal backend is running",
+  });
 });
+
+app.get("/api/health", (_req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: "BUP FST Journal API is healthy",
+  });
+});
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  });
+});
+
+app.use(
+  (
+    error: unknown,
+    _req: Request,
+    res: Response,
+    _next: NextFunction
+  ) => {
+    console.error("Server Error:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
+);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,6 @@
 import app from "./app";
 import { env } from "./config/env";
-import { connectDB } from "./config/db";
-import { seedAdmin } from "./utils/seedAdmin";
-import { seedPages } from "./utils/seedPages";
-import { seedHomepage } from "./utils/seedHomepage";
 
-const startServer = async () => {
-  await connectDB();
-
-  await seedAdmin();
-  await seedPages();
-  await seedHomepage();
-
-  app.listen(env.port, () => {
-    console.log(`Server running on http://localhost:${env.port}`);
-  });
-};
-
-startServer();
+app.listen(env.port, () => {
+  console.log(`Server running on http://localhost:${env.port}`);
+});
